2186ab80e6547361e60ba8896fe6fd1bb014cc85
agrawalTanish <157560604+agrawalTanish@users.noreply.github.com>
2026-05-24 12:58:11 +0530

    Time: 0 ms (100.00%) | Memory: 9.1 MB (83.48%) - LeetSync

---
+++ 316-remove-duplicate-letters/remove-duplicate-letters.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    string removeDuplicateLetters(string s) {
+
+        vector<int> freq(26, 0);
+        vector<bool> visited(26, false);
+
+        for (char ch : s) {
+            freq[ch - 'a']++;
+        }
+
+        string ans = "";
+
+        for (char ch : s) {
+
+            freq[ch - 'a']--;
+
+            if (visited[ch - 'a']) {
+                continue;
+            }
+
+            while (!ans.empty() &&
+                   ans.back() > ch &&
+                   freq[ans.back() - 'a'] > 0) {
+
+                visited[ans.back() - 'a'] = false;
+                ans.pop_back();
+            }
+
+            ans.push_back(ch);
+            visited[ch - 'a'] = true;
+        }
+
+        return ans;
+    }
+};
